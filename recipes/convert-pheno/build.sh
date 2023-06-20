2349682c883c19c46a6c644ea3361e0824009511
Ivo Leist <ivo.leist@googlemail.com>
2023-06-20 19:37:14 +0200

    try to only install PerlIO::gzip

--- recipes/convert-pheno/build.sh
+++ recipes/convert-pheno/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-HOME=/tmp cpanm --installdeps .
-git clone https://github.com/tkluck/pyperler.git
-cd pyperler
-python setup.py build && sudo python setup.py install
+HOME=/tmp cpanm PerlIO::gzip
+# HOME=/tmp cpanm --installdeps .
+# git clone https://github.com/tkluck/pyperler.git
+# cd pyperler
+# python setup.py build && sudo python setup.py install
