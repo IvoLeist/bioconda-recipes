145e542423f39daee660e00b7f5d0e8e1a308b0d
Ivo Leist <ivo.leist@googlemail.com>
2023-06-25 17:15:53 +0200

    try again after c_include_path modification

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # HOME=/tmp cpanm --installdeps .
 export LD_LIBRARY_PATH="${PREFIX}/lib"
-export C_INCLUDE_PATH="${PREFIX}/include"
+# export C_INCLUDE_PATH="${PREFIX}/include"
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
 # HOME=/tmp cpanm PerlIO::gzip
 HOME=/tmp cpanm Convert::Pheno
 
