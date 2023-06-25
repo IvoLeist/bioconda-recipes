c01496e8a652eb3f0198a57c0175d9db165a996a
Ivo Leist <ivo.leist@googlemail.com>
2023-06-25 15:57:41 +0200

    try with setting C_INCLUDE_PATH

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # HOME=/tmp cpanm --installdeps .
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+export C_INCLUDE_PATH="${PREFIX}/include"
 HOME=/tmp cpanm PerlIO::gzip
 # HOME=/tmp cpanm  Convert::Pheno
+
+# perl Makefile.PL INSTALLDIRS=site \
+#     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
+# make
+# make test
+# make install
