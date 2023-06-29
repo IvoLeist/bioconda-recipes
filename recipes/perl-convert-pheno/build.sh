1e25874d8e701f3563b12443447fc6aecbaa6cb2
Ivo Leist <ivo.leist@googlemail.com>
2023-06-29 18:54:26 +0200

    added to be installed deps

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -8,8 +8,21 @@
 # export C_INCLUDE_PATH=${C_INCLUDE_PATH}:/usr/include/x86_64-linux-gnu
 # HOME=/tmp cpanm PerlIO::gzip
 
-perl Makefile.PL INSTALLDIRS=site
+# install dependencies not found in conda channels
+HOME /tmp cpanm File::ShareDir::ProjectDistDir
+HOME /tmp cpanm JSON::Validator
+HOME /tmp cpanm Moo
+HOME /tmp cpanm Path::Tiny
+HOME /tmp cpanm Test::Deep
+HOME /tmp cpanm Test::Warn
+HOME /tmp cpanm Text::CSV_XS
+HOME /tmp cpanm Text::Similarity
+HOME /tmp cpanm Types::Standard
+HOME /tmp cpanm XML::Fast
+HOME /tmp cpanm YAML::XS
 
+
+perl Makefile.PL INSTALLDIRS=site
 # perl Makefile.PL INSTALLDIRS=site \
 #     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
 make
