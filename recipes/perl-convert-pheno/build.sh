eafb8821742cd71b140e170cc74cce1480161f5f
Ivo Leist <ivo.leist@googlemail.com>
2023-06-29 18:12:23 +0200

    try to build convert-pheno from makefile

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-HOME=/tmp cpanm Convert::Pheno
+# HOME=/tmp cpanm Convert::Pheno
 # HOME=/tmp cpanm --installdeps .
 # export LD_LIBRARY_PATH="${PREFIX}/lib"
 # export C_INCLUDE_PATH="${PREFIX}/include"
 
 # below causes a lot of compilation errors
 # export C_INCLUDE_PATH=${C_INCLUDE_PATH}:/usr/include/x86_64-linux-gnu
+# HOME=/tmp cpanm PerlIO::gzip
+
+perl Makefile.PL INSTALLDIRS=site \
+    INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
+make
+make test
+make install
