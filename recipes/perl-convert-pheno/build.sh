e39e16ca4be760f890e6cb2fb03457739122a152
Ivo Leist <ivo.leist@googlemail.com>
2023-06-29 18:03:37 +0200

    changed version to 0.09

--- recipes/perl-convert-pheno/build.sh
+++ recipes/perl-convert-pheno/build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
+HOME=/tmp cpanm Convert::Pheno
 # HOME=/tmp cpanm --installdeps .
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+# export LD_LIBRARY_PATH="${PREFIX}/lib"
 # export C_INCLUDE_PATH="${PREFIX}/include"
-find "${PREFIX}/include" -name "timesize.h"
-export C_INCLUDE_PATH=${C_INCLUDE_PATH}:"${PREFIX}/include"
 
 # below causes a lot of compilation errors
 # export C_INCLUDE_PATH=${C_INCLUDE_PATH}:/usr/include/x86_64-linux-gnu
-# HOME=/tmp cpanm PerlIO::gzip
-HOME=/tmp cpanm Convert::Pheno
-
-# perl Makefile.PL INSTALLDIRS=site \
-#     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
-# make
-# make test
-# make install
